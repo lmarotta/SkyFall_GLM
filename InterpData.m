f2991915f37e7a8db66a875268964eb890c48e34
nshawen <nicholasshawen2013@u.northwestern.edu>
2016-09-01 11:02:59 -0500

    updated features from interpolated data

--- InterpData.m
+++ InterpData.m
@@ -6,7 +6,7 @@ t=0:.02:4.98;
 tbar=0:1/6:29/6;
 
 for i=1:length(labels.acce)
-    if size(labels.acce{i},1)>99
+    if size(labels.acce{i},1)>99 && range(labels.acce{i}(:,1))>4
         data=labels.acce{i};
         
         % remove duplicates
@@ -14,8 +14,9 @@ for i=1:length(labels.acce)
         data(diff(data(:,1))==0,:)=[];
         
         labels.acce{i}=[t' spline(data(:,1)'-data(1,1),data(:,2:end)',t')'];
+    else labels.acce{i}=[];
     end
-    if size(labels.gyro{i},1)>99
+    if size(labels.gyro{i},1)>99 && range(labels.gyro{i}(:,1))>4
         data=labels.gyro{i};
         
         % remove duplicates
@@ -23,8 +24,9 @@ for i=1:length(labels.acce)
         data(diff(data(:,1))==0,:)=[];
         
         labels.gyro{i}=[t' spline(data(:,1)'-data(1,1),data(:,2:end)',t')'];
+    else labels.gyro{i}=[];
     end
-    if size(labels.baro{i},1)>9
+    if size(labels.baro{i},1)>9 && range(labels.baro{i}(:,1))>4
         data=labels.baro{i};
         
         % remove duplicates
@@ -32,6 +34,7 @@ for i=1:length(labels.acce)
         data(diff(data(:,1))==0,:)=[];
         
         labels.baro{i}=[tbar' spline(data(:,1)'-data(1,1),data(:,2:end)',tbar')'];
+    else labels.baro{i}=[];
     end
 end
 
