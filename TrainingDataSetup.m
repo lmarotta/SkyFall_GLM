636aeaa059b068131e0a067ddfbf7dfcb327e06a
Nick Shawen <nicholasshawen2013@u.northwestern.edu>
2016-11-02 10:49:16 -0500

    updated TrainingDataSetup.m

--- TrainingDataSetup.m
+++ TrainingDataSetup.m
@@ -1,4 +1,18 @@
 %% TrainingDataSetup
+% Inputs:
+% locations     Cell array of desired data locations {'pouch', 'pocket', 'hand'}
+% subjs         Cell array of desired subject IDs
+%
+% leave inputs empty to use all
+
+function TrainingDataSetup(locations, subjs, n)
+
+if nargin<2
+    subjs=[];
+end
+if nargin<1
+    locations=[];
+end    
 
 filenames=dir('falls_data_10sec_*.mat');
 
@@ -20,7 +34,22 @@ for indF=1:length(filenames)
     
 end
 
+%% Remove undesired subjects and locations
+if ~isempty(locations)
+    loc_inds=cellfun(@(x) any(strcmp(x,locations)),data.location);
+else
+    loc_inds=ones(length(data.location),1);
+end
+if ~ isempty(subjs)
+    subj_inds=cellfun(@(x) any(strcmp(x,subjs)),data.subject);
+else
+    subj_inds=ones(length(data.subject),1);
+end
+
 empty_inds=cellfun(@isempty, data.acce) | cellfun(@isempty, data.gyro) | cellfun(@isempty, data.baro);
+
+empty_inds=empty_inds | ~loc_inds | ~subj_inds;
+
 data.acce(empty_inds)=[];
 data.gyro(empty_inds)=[];
 data.baro(empty_inds)=[];
@@ -29,7 +58,7 @@ data.subject(empty_inds)=[];
 data.location(empty_inds)=[];
 data.type_str(empty_inds)=[];
 
-% separate falls and non-falls
+%% separate falls and non-falls
 fall_inds=data.value~=9;
 falls_data.acce=data.acce(fall_inds);
 falls_data.gyro=data.gyro(fall_inds);
@@ -47,7 +76,7 @@ act_data.subject=data.subject(~fall_inds);
 act_data.location=data.location(~fall_inds);
 act_data.type_str=data.type_str(~fall_inds);
 
-falls_data=JitterClips_new(falls_data,10);
+falls_data=JitterClips_new(falls_data,n);
 
 data.acce=[falls_data.acce; act_data.acce];
 data.gyro=[falls_data.gyro; act_data.gyro];
@@ -56,6 +85,8 @@ data.value=[falls_data.value; act_data.value];
 data.subject=[falls_data.subject; act_data.subject];
 data.location=[falls_data.location; act_data.location];
 data.type_str=[falls_data.type_str; act_data.type_str];
+
+%% Interpolate Data
 
 [acce, gyro, baro]=InterpData(data);
 
@@ -71,11 +102,6 @@ data.value(empty_inds)=[];
 data.subject(empty_inds)=[];
 data.location(empty_inds)=[];
 data.type_str(empty_inds)=[];
-
-%% Remove duplicates
-
-
-
 
 labels=data;
 save Training_Data labels
