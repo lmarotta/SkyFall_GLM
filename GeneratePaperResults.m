0d47867f21d7450abe9a7a4b5f6eff2abc246fe3
ingluca@gmail.com <ingluca@gmail.com>
2017-01-13 17:57:52 -0600

    added AUC computation for RF

--- GeneratePaperResults.m
+++ GeneratePaperResults.m
@@ -4,7 +4,7 @@
 % Input: Features set (0:reduced, 1:full)
          %Number of locations (1:waist, 2:waist+pocket, 3:all)
 % Output: AUC, Sens, Spec
-function GeneratePaperResults
+function results = GeneratePaperResults
 
 % close all
 %% Store Amputees data (all 3 locations) for testing
@@ -25,12 +25,12 @@ else
     X = X.F;
 end
 
-% [AUC{2},Sens{2},Spec{2}] = LOSOCV(X,X_Amp,1,1,0);
-% [AUC{4},Sens{4},Spec{4}] = LOSOCV(X,X_Amp,2,1,0);
-% [AUC{6},Sens{6},Spec{6}] = LOSOCV(X,X_Amp,3,1,0);
-[AUC{1},Sens{1},Spec{1}] = LOSOCV(X,X_Amp,1,1,1);
-[AUC{3},Sens{3},Spec{3}] = LOSOCV(X,X_Amp,2,1,1);
-[AUC{5},Sens{5},Spec{5}] = LOSOCV(X,X_Amp,3,1,1);
+[AUC{2},Sens{2},Spec{2}] = LOSOCV(X,X_Amp,1,0,0); %RF
+[AUC{4},Sens{4},Spec{4}] = LOSOCV(X,X_Amp,2,0,0);
+[AUC{6},Sens{6},Spec{6}] = LOSOCV(X,X_Amp,3,0,0);
+[AUC{1},Sens{1},Spec{1}] = LOSOCV(X,X_Amp,1,1,0); %GLMnet
+[AUC{3},Sens{3},Spec{3}] = LOSOCV(X,X_Amp,2,1,0);
+[AUC{5},Sens{5},Spec{5}] = LOSOCV(X,X_Amp,3,1,0);
 
 mAUC=cellfun(@mean,AUC);
 sAUC=cellfun(@std,AUC);
@@ -46,6 +46,10 @@ mSpec=cellfun(@mean,Spec);
 sSpec=cellfun(@std,Spec);
 
 figure; hold on; bar(mSpec); for i=1:6; errorbar(i,mSpec(i), 0, sSpec(i),'Color','k'); end
+
+results.AUC = AUC;
+results.Sens = Sens;
+results.Spec = Spec;
 
 end 
 
@@ -97,6 +101,7 @@ for indCV=1:length(subj)
     falls_ind = find(indtrain & L);
     nfalls_ind = find(indtrain & ~L);
     
+    %balance the dataset 
     indtrain = [falls_ind(randperm(length(falls_ind),500)); nfalls_ind(randperm(length(nfalls_ind),500))];
     
     if model
@@ -115,11 +120,16 @@ for indCV=1:length(subj)
         [~, ~, ~, AUC(indCV)]=perfcurve(isfall, conf, true);
         Sens(indCV) = sum(pred & isfall)/sum(isfall);
         Spec(indCV) = sum(~pred & ~isfall)/sum(~isfall);
-    else
+    else 
         RFModel=TreeBagger(100,F(indtrain,:),L(indtrain));
-        pred=predict(RFModel,F(indtest,:));
+        [pred,conf]=predict(RFModel,F(indtest,:));
+        conf = conf(:,2); %posterior prob of a fall
+        isfall = logical(L(indtest));
+        isfall_all{indCV}=isfall;
+        [~, ~, ~, AUC(indCV)]=perfcurve(isfall, conf, true);
         
         isfall = logical(L(indtest));
+        isfall_all{indCV}=isfall;
         pred=cellfun(@(x) logical(str2double(x)),pred);
         
         confmat=confusionmat(isfall,pred);
@@ -157,8 +167,9 @@ if ~isempty(X_test)
 %         figure; plot(X,Y);
     else
         RFModel=TreeBagger(100,F(indtrain,:),L(indtrain));
-        pred=predict(RFModel,Ft);
-        
+        [pred,conf]=predict(RFModel,Ft);
+        conf = conf(:,2); %posterior prob of a fall
+
         confmat=confusionmat(X_test(:,4)<9,cellfun(@(x) logical(str2double(x)),pred));
         plotConfmat(confmat);
     end
@@ -166,7 +177,7 @@ if ~isempty(X_test)
 end
 
 if model
-    [X, Y, ~]=perfcurve(isfall_all, conf_all, true,'XVals',[0:0.05:1]); %skip subject 1 (CK) data
+    [X, Y, ~]=perfcurve(isfall_all, conf_all, true,'XVals',[0:0.05:1]);
 %     figure; errorbar(X,Y(:,1),Y(:,1)-Y(:,2),Y(:,3)-Y(:,1));
 end
 
